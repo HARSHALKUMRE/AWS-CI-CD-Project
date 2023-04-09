599a7834a41de3ea67893bb45d6cd76bbd65faf5
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-04-09 15:04:30 +0530

    data ingestion file updated

--- student/components/data_ingestion.py
+++ student/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from student.components.data_transformation import DataTransformationConfig
+from student.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -43,8 +45,11 @@ class DataIngestion:
 
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise StudentException(e,sys)
         
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
